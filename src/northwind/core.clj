3a0835e2360d92f6a1ed738ab604cac03fd88c60
Kwabena (Kay) Bosompem <kbosompem@gmail.com>
2023-08-03 19:00:28 -0500

    First working schema

--- src/northwind/core.clj
+++ src/northwind/core.clj
@@ -1,71 +1,194 @@
 (ns northwind.core
-  (:refer-clojure :exclude [load])
-  (:require [pod.huahaiy.datalevin :as d]))
+  (:require [pod.huahaiy.datalevin :as d]
+            [clojure.pprint :as pp])
+  (:import [java.util Calendar]
+           [java.util Date]))
 
-(def northwind-schema (read-string (slurp "schema.edn")))
+(def schema-files [:category :customer :employee
+                   :region 
+                   :territory 
+                   :supplier 
+                   :shipper  
+                   :product 
+                   :order
+                   :orderdetail  
+                   :demographic
+                   ])
 
-(def northwind-data (read-string (slurp "data.edn")))
+; -- Delete DB
+(.delete (java.io.File. "n1/data.mdb"))
+(.delete (java.io.File. "n1/lock.mdb"))
 
-(def conn (d/get-conn "northwind" northwind-schema))
+(def northwind-schema (apply merge (map #(read-string (slurp (str "src/northwind/schema/" (name %) ".edn"))) schema-files)))
+
+(def conn (d/get-conn "n1" northwind-schema))
 
 (def db (d/db conn))
 
+(def northwind-data (read-string (slurp "data.edn")))
+
 (d/transact! conn northwind-data)
 
+; -- UTILITY FUNCTIONS 
+(defn get-year [dt]
+  (let [cal (Calendar/getInstance)
+        _ (.setTime cal dt)]
+    (.get cal Calendar/YEAR)))
 
-(clojure.pprint/pprint
- (d/q '[:find  ?a (count ?e)
-        :where
-        [?e ?a _]]
-      db))
+(def pt pp/print-table)
+
+; -- QUERIES 
+; -- https://www.geeksengine.com/database/problem-solving/northwind-queries-part-1.php
+
+(defn order-subtotals 
+  "Order SubTotals
+   
+   (pt (sort-by :orderid (order-subtotals)))"
+  []
+  (d/q '[:find  ?o (sum ?s)
+         :keys orderid subtotal
+         :where
+         [?e :orderdetail/order ?o]
+         [?e :orderdetail/quantity ?q]
+         [?e :orderdetail/unitprice ?up]
+         [?e :orderdetail/discount ?d]
+         [(- 1 ?d) ?x]
+         [(* ?up ?q ?x) ?s]] db))
+
+(defn sales-by-year
+  "Sales by Year
+   
+   (pt (sort-by :orderid (sales-by-year)))"
+  []
+  (d/q '[:find ?sd ?o (sum ?s) ?yr
+         :keys shippeddate orderid subtotal year
+         :where
+         [?e :orderdetail/order ?o]
+         [?e :orderdetail/quantity ?q]
+         [?e :orderdetail/unitprice ?up]
+         [?e :orderdetail/discount ?d]
+         [?o :order/shippeddate ?sd]
+         [(get-year ?sd) ?yr]
+         [(- 1 ?d) ?x]
+         [(* ?up ?q ?x) ?s]] db))
+
+
+
 
 (comment
-  ; Get Employee and their Boss
+(println :bad-attributes
+         (remove second (d/q '[:find   ?e ?a ?v
+                               :where
+                               [?e ?a ?v]] db)))
+ 
   (clojure.pprint/pprint
-   (d/q '[:find (pull ?e [[:employee/firstname :as :fname]
-                          [:employee/lastname :as :lname]
-                          [:db/id :as :id]
-                          {[:territory/territory :as :territories] [[:territory/description]]}
-                          {[:employee/reportsto :as :boss]
-                           [[:employee/firstname :as :fname]
+    (sort-by first (d/q '[:find  ?o (sum ?s)
+                          :where
+                          [?e :orderdetail/order ?o]
+                          [?e :orderdetail/quantity ?q]
+                          [?e :orderdetail/unitprice ?up]
+                          [?e :orderdetail/discount ?d]
+                          [(- 1 ?d) ?x]
+                          [(* ?up ?q ?x) ?s]] db)))
+  
+
+   (d/q '[:find  (pull ?e [[:orderdetail/order]])
+                        :where
+                        [?e :orderdetail/order ?o]
+                        [?e :orderdetail/quantity ?q]
+                        [?e :orderdetail/unitprice ?up]
+                        [?e :orderdetail/discount ?d]
+                        [(- 1 ?d) ?x]
+                        [(* ?up ?q ?x) ?s]] db)
+
+    (d/q '[:find  ?a (count ?e) :where [?e ?a _]] db)
+    (d/q '[:find  ?a (count ?e) :where [?e ?a _]] db)
+    (d/q '[:find  (count ?e) :where [?e]] db)
+    (d/q '[:find (pull ?e [*])
+           :where
+           [?e :shipper/companyname]] db)
+
+    (:product/productname northwind-schema)
+    (d/transact! conn [{:order/shipvia "3"
+                        :order/shippostalcode "51100"
+                        :order/shipaddress "59 rue de l'Abbaye"
+                        :order/shipname "Vins et alcools Chevalier"
+                        :order/requireddate #inst "1996-08-01T05:00:00.000-00:00"
+                        :order/freight 32.38
+                        :order/employee  104
+                        :db/id  -40248
+                        :order/shippeddate #inst "1996-07-16T05:00:00.000-00:00"
+                        :order/customer [:customer/customerid   "VINET"]
+                        :order/shipregion "NULL"
+                        :order/orderdate #inst "1996-07-04T05:00:00.000-00:00"
+                        :order/shipcity "Reims"}])
+  ; Get Employee and their Boss
+    (clojure.pprint/pprint
+     (d/q '[:find (pull ?e [[:employee/firstname :as :fname]
                             [:employee/lastname :as :lname]
-                            [:db/id :as :id]]}])
-          :where
-          [?e :employee/title]]
-        db))
+                            [:db/id :as :id]
+                            {[:territory/territory :as :territories] [[:territory/description]]}
+                            {[:employee/reportsto :as :boss]
+                             [[:employee/firstname :as :fname]
+                              [:employee/lastname :as :lname]
+                              [:db/id :as :id]]}])
+            :where
+            [?e :employee/title]]
+          db))
+
+    (d/q '[:find ?e ?a ?v
+           :where
+           [?e :order/shipname]
+           [?e ?a ?v]] db)
+    (clojure.pprint/print-table
+     (map first (d/q '[:find   (pull ?e [*])
+                       :where
+                       [?e :order/shipname]] db)))
+
+    (remove second (d/q '[:find   ?e ?a ?v
+                          :where
+                          [?e ?a ?v]] db))
+
+    (d/q '[:find    ?a ?v
+           :where
+           [23 ?a ?v]] db)
 
   ; Employees
-  (d/q '[:find   (pull ?e [*])
-         :where
-         [?e :employee/title]] db)
+    (d/q '[:find   (pull ?e [*])
+           :where
+           [?e :employee/title]] db)
 
   ; Suppliers
-  (clojure.pprint/print-table
-   (map first (d/q '[:find   (pull ?e [*])
-                     :where
-                     [?e :supplier/companyname]] db)))
-  #{}
-  
-  (def z {:db/id -20002,
-          :employee/photo
-          "0x151C2F00020000000D000E0014002100FFFFFFFF4269746D617020496D616765005061696E742E506963747572650001050000020000000700000050427275736800000000000000000020540000424D20540000000000007600000028000000C0000000DF0000000100040000000000A0530000CE0E0000D80E0000000000",
-          :employee/address "908 W. Capital Way",
-          :employee/hiredate #inst "1992-08-14T05:00:00.000-00:00",
-          :employee/notes
-          "Andrew received his BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  He is fluent in French and Italian and reads German.  He joined the company as a sales representative, was promoted to sales manager i",
-          :employee/region "WA",
-          :employee/city "Tacoma",
-          :employee/titleofcourtesy "Dr.",
-          :employee/birthdate #inst "1952-02-19T06:00:00.000-00:00",
-          :employee/lastname "Fuller",
-          :employee/phoneextension "3457",
-          :employee/photopath "http://accweb/emmployees/fuller.bmp",
-          :employee/postalcode "98401",
-          :employee/homephone "(206) 555-9482",
-          :employee/firstname "Andrew",
-          :employee/country "USA",
-          :employee/title "Vice President, Sales"})
-  
-  (-> z  keys sort)
-  
-  )
+    (clojure.pprint/print-table
+     (map first (d/q '[:find   (pull ?e [*])
+                       :where
+                       [?e :supplier/companyname]] db)))
+
+  ; Shippers
+    (clojure.pprint/print-table
+     (map first (d/q '[:find   (pull ?e [*])
+                       :where
+                       [?e :shipper/companyname]] db)))
+
+   ; Products
+    (clojure.pprint/print-table
+     (map first (d/q '[:find   (pull ?e [[:product/productname :as product]])
+                       :where
+                       [?e :product/productname]] db)))
+
+
+    (d/transact! conn [{:order/shipvia "3"
+                        :order/shippostalcode "51100"
+                        :order/shipaddress "59 rue de l'Abbaye"
+                        :order/shipname "Vins et alcools Chevalier"
+                        :order/shipcountry "France"
+                        :order/requireddate #inst "1996-08-01T05:00:00.000-00:00"
+                        :order/freight 32.38
+                        :order/employee  [:employee/firstname "Adam"]
+                      ;:db/id  -40248
+                        :order/shippeddate #inst "1996-07-16T05:00:00.000-00:00"
+                        :order/customer [:customer/customerid   "VINET"]
+                        :order/shipregion "NULL"
+                        :order/orderdate #inst "1996-07-04T05:00:00.000-00:00"
+                        :order/shipcity "Reims"}])
+    #{})
